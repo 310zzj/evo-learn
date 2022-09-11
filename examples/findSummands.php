0c12e615fa692ce2908969e39e8e727fdd8c6a36
310zzj <310zzj@proton.me>
2022-09-11 20:41:20 +0000

    Update sample project

--- examples/findSummands.php
+++ examples/findSummands.php
@@ -8,4 +8,9 @@
 	header("Content-type: text/plain; charset=utf-8");
 	include "../evolutive.class.php";
 
-	class FindSummandsSpecimen extends Evol
+	class FindSummandsSpecimen extends Evolutive\Specimen {
+		public $desiredResult = 10;
+		public $result;
+		
+		public function FindSummandsSpecimen($dna = false) {
+			$thi
