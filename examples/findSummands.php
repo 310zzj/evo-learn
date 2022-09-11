9f594a1b0834f77d872741112ff1b55c009201a2
310zzj <310zzj@proton.me>
2022-09-11 10:38:42 +0000

    New translations app.dev.po (Galician)

--- examples/findSummands.php
+++ examples/findSummands.php
@@ -2,4 +2,7 @@
 
 	/**
 	 * Example of Evolutive https://github.com/tin-cat/evolutive, A library to learn about simple evolutive, genetics-based machine learning algorithms and object-oriented programming by Tin.cat
-	 * Using a
+	 * Using an evolutive, genetics-based machine learning algorithm to find the two sumands that give a specific result.
+	 */
+
+	header("Content-ty
